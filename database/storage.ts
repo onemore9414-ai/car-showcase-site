7aad6701ad8e1ba2e6b90ec347cde8d5d1b34c59
onemore9414-ai <onemore9414@gmail.com>
2026-02-18 09:33:01 +0500

    Update storage.ts

--- database/storage.ts
+++ database/storage.ts
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // This tells the code to look for variables in Vercel's settings
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.VITE_SUPABASE_URL!;
+const supabaseKey = process.env.VITE_SUPABASE_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
