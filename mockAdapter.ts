705f1be139a136e2a59c9b11235640d74db5dafc
onemore9414-ai <onemore9414@gmail.com>
2026-02-18 09:12:04 +0500

    Create mockAdapter.ts

---
+++ mockAdapter.ts
@@ -0,0 +1,6 @@
+export const mockAdapter = {
+  get: async () => ({ data: null }),
+  post: async () => ({ data: null }),
+  put: async () => ({ data: null }),
+  delete: async () => ({ data: null }),
+};
